52277a3908e515c648077a20f71569b93af578c1
Dinesh Sivalanka <sivalankadinesh2005@gmail.com>
2026-05-10 11:33:20 +0530

    Add activities API

--- Backend/app.py
+++ Backend/app.py
@@ -75,5 +75,82 @@ def get_trips():
 
     return jsonify(trips)
 
+@app.route("/stops", methods=["POST"])
+def add_stop():
+    data = request.get_json()
+
+    trip_id = data["trip_id"]
+    city_name = data["city_name"]
+    arrival_date = data["arrival_date"]
+    departure_date = data["departure_date"]
+    stop_order = data["stop_order"]
+
+    conn = connect_db()
+    cur = conn.cursor()
+
+    cur.execute(
+        "INSERT INTO trip_stops (trip_id, city_name, arrival_date, departure_date, stop_order) VALUES (%s, %s, %s, %s, %s)",
+        (trip_id, city_name, arrival_date, departure_date, stop_order)
+    )
+
+    conn.commit()
+    cur.close()
+    conn.close()
+
+    return {"message": "Stop added"}
+
+@app.route("/stops/<int:trip_id>", methods=["GET"])
+def get_stops(trip_id):
+    conn = connect_db()
+    cur = conn.cursor()
+
+    cur.execute("SELECT * FROM trip_stops WHERE trip_id = %s ORDER BY stop_order;", (trip_id,))
+    stops = cur.fetchall()
+
+    cur.close()
+    conn.close()
+
+    return jsonify(stops)
+
+@app.route("/activities", methods=["POST"])
+def add_activity():
+    data = request.get_json()
+
+    conn = connect_db()
+    cur = conn.cursor()
+
+    cur.execute(
+        "INSERT INTO activities (stop_id, activity_name, activity_date, estimated_cost, notes) VALUES (%s, %s, %s, %s, %s)",
+        (
+            data["stop_id"],
+            data["activity_name"],
+            data["activity_date"],
+            data["estimated_cost"],
+            data["notes"]
+        )
+    )
+
+    conn.commit()
+    cur.close()
+    conn.close()
+
+    return {"message": "Activity added"}
+
+@app.route("/activities/<int:stop_id>", methods=["GET"])
+def get_activities(stop_id):
+    conn = connect_db()
+    cur = conn.cursor()
+
+    cur.execute(
+        "SELECT * FROM activities WHERE stop_id = %s;",
+        (stop_id,)
+    )
+    activities = cur.fetchall()
+
+    cur.close()
+    conn.close()
+
+    return jsonify(activities)
+
 if __name__ == "__main__":
     app.run(debug=True)
